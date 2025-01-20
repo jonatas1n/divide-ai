9c9f2b23c7a0c2bca1a4413c9cae42419e8c6cd8
jonatas1n <jonatasgeek@gmail.com>
2025-01-20 15:34:53 -0300

    chore: minor

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import { Container } from '../../components/Container';
 
