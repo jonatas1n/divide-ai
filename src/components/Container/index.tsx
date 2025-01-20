43d8c4b5d7d17828afbb97cf2792510e70ecf963
jonatas1n <jonatasgeek@gmail.com>
2025-01-20 15:33:22 -0300

    chore: replace custom container by MUI component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Stack from "@mui/material/Stack";
+import MUIContainer from "@mui/material/Container";
 import { Header } from "../../components/Header";
 
 type ContainerProps = {
@@ -11,9 +12,9 @@ export const Container = ({ children, title }: ContainerProps) => {
   return (
     <Stack m={0}>
       <Header title={title} />
-      <Stack maxWidth="1366px" sx={{width: {xs: "95%", md: "60%"}}} marginInline="auto">
+      <MUIContainer>
         {children}
-      </Stack>
+      </MUIContainer>
     </Stack>
   )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,7 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { Menu } from "@mui/icons-material";
 import { useState } from "react";
+import { Container } from "@mui/material";
 
 const routes = {
   guests: "Participantes",
@@ -22,51 +23,50 @@ export const Header = ({ title }: HeaderProps) => {
 
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
-      <Stack
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        p={2}
-        maxWidth="1366px"
-        sx={{ width: { xs: "95%", md: "60%" } }}
-        marginInline="auto"
-      >
-        <Typography variant="h4">{title}</Typography>
-        <Stack justifyContent="center">
-          <Button
-            color="inherit"
-            onClick={toggleMenu}
-            sx={{ display: { xs: "block", md: "none" } }}
-          >
-            <Menu />
-          </Button>
-          <Stack
-            zIndex={5}
-            right={0}
-            top={64}
-            bgcolor="#1976D2"
-            sx={{
-              flexDirection: { xs: "column", md: "row" },
-              display: { xs: showMenu ? "flex" : "none", md: "flex" },
-              position: { xs: "absolute", md: "unset" },
-            }}
-          >
-            {Object.entries(routes).map(([path, label]) => (
-              <Button
-                key={path}
-                href={path}
-                sx={{
-                  color: "white",
-                  display: "block",
-                  textDecoration: "underline",
-                }}
-              >
-                {label}
-              </Button>
-            ))}
+      <Container>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          p={1}
+        >
+          <Typography variant="h4">{title}</Typography>
+          <Stack justifyContent="center">
+            <Button
+              color="inherit"
+              onClick={toggleMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              <Menu />
+            </Button>
+            <Stack
+              zIndex={5}
+              right={0}
+              top={64}
+              bgcolor="#1976D2"
+              sx={{
+                flexDirection: { xs: "column", md: "row" },
+                display: { xs: showMenu ? "flex" : "none", md: "flex" },
+                position: { xs: "absolute", md: "unset" },
+              }}
+            >
+              {Object.entries(routes).map(([path, label]) => (
+                <Button
+                  key={path}
+                  href={path}
+                  sx={{
+                    color: "white",
+                    display: "block",
+                    textDecoration: "underline",
+                  }}
+                >
+                  {label}
+                </Button>
+              ))}
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
+      </Container>
     </AppBar>
   );
 };
