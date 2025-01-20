da97ce46afb907b36d6519a5ba4e711032b2b4ba
jonatas1n <jonatasgeek@gmail.com>
2025-01-20 03:53:14 -0300

    feat: add new result page

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,8 +1,18 @@
-import { Container } from "../../components/common/Container"
-import { Header } from "../../components/common/Header"
-import { Content } from "../../components/common/Content"
+import { Container } from "../../components/common/Container";
+import { Header } from "../../components/common/Header";
+import { Content } from "../../components/common/Content";
+import { useAppContext } from "../../hooks/Context";
+import { ItemStyle } from "../../components/common/Item";
+
+import { useEffect } from "react";
 
 export const Result = () => {
+  const { guests, costs, products, calculateTotalCosts } = useAppContext();
+
+  useEffect(() => {
+    calculateTotalCosts();
+  }, [costs, products, guests, calculateTotalCosts]); 
+
   return (
     <Container>
       <Header>
@@ -12,8 +22,12 @@ export const Result = () => {
         <h1>Resultado</h1>
       </Header>
       <Content>
-        Resultado
+        {Object.values(guests).map( guest =>
+          <ItemStyle key={guest.id}>
+            {guest.name}: R$ {guest.totalCost?.toFixed(2)}
+          </ItemStyle>
+        )}
       </Content>
     </Container>
-  )
-}
+  );
+};
