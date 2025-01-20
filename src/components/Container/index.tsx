355fb603a39eded1a2ad10028b7ecac2e39c796b
jonatas1n <jonatasgeek@gmail.com>
2025-01-20 05:14:13 -0300

    feat: replace styled components from container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,19 +1,14 @@
-import { ReactNode } from "react"
-import styled from "styled-components"
+import { ReactNode } from "react";
+import Stack from "@mui/material/Stack";
 
 type ContainerProps = {
   children: ReactNode
 }
 
-const ContainerStyle = styled.div`
-  max-width: 1366px;
-  width: 60%;
-  margin-inline: auto;
-  font-family: sans-serif;
-`;
-
 export const Container = ({ children }: ContainerProps) => {
   return (
-    <ContainerStyle>{children}</ContainerStyle>
+    <Stack maxWidth="1366px" width="60%" marginInline="auto">
+      {children}
+    </Stack>
   )
 }
