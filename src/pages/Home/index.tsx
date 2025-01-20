8e39e0f3b3f9fd6334ea0b3fb73f3132f3faa24c
jonatas1n <jonatasgeek@gmail.com>
2025-01-20 02:46:04 -0300

    fix: change page link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ export const Home = () => {
   return (
     <>
       <>Bem vindo a calculadora do bar! PErfeita pra ninguém pagar a mais</>
-      <a href="/products">Começar</a>
+      <a href="/guests">Começar</a>
     </>
   )
 }
