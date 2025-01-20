5b0bbfd5fc9a3cfb5018a7984ec7be8d9da2db53
jonatas1n <jonatasgeek@gmail.com>
2025-01-20 05:33:40 -0300

    chore: replace string by label param

--- src/components/guests/GuestForm/index.tsx
+++ src/components/guests/GuestForm/index.tsx
@@ -7,29 +7,29 @@ import TextField from "@mui/material/TextField";
 
 type GuestFormProps = {
   guest: GuestType;
-}
+};
 
 export const GuestForm = ({ guest }: GuestFormProps) => {
   const [formData, setFormData] = useState<GuestType>(guest);
   const { changeGuest } = useAppContext();
 
-  const handleChange = (field: keyof GuestType) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
-    const updatedProduct = { ...formData, [field]: value };
-    setFormData(updatedProduct);
-    changeGuest(updatedProduct);
-  };
+  const handleChange =
+    (field: keyof GuestType) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      const updatedProduct = { ...formData, [field]: value };
+      setFormData(updatedProduct);
+      changeGuest(updatedProduct);
+    };
 
   return (
     <Stack>
-      <Stack direction="row">
-        Nome:
-        <TextField
-          value={formData.name}
-          onChange={handleChange("name")}
-          required
-        />
-      </Stack>
+      <TextField
+        label="Nome"
+        value={formData.name}
+        onChange={handleChange("name")}
+        required
+      />
     </Stack>
   );
 };
