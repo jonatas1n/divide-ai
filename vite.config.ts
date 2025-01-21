e7bb33725a4b5aa4c1e398ef44400d25a8853b72
jonatas1n <jonatasgeek@gmail.com>
2025-01-21 03:39:33 -0300

    feat: make this app a PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Conta-bar',
+        short_name: 'ContaBar',
+        description: 'Uma aplicação para dividir contas de bares e restaurantes',
+        theme_color: '#1976d2',
+        start_url: "/?fullscreen=true",
+        display: "fullscreen",
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
