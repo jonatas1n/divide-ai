170ca950cfcd1583340749460178525d24580307
jonatas1n <jonatasgeek@gmail.com>
2025-01-21 11:20:27 -0300

    feat: use logo as link to homepage

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,6 +13,7 @@ import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 import Calculate from "@mui/icons-material/Calculate";
 import Close from "@mui/icons-material/Close";
 import Box from "@mui/material/Box";
+import { Link } from "@mui/material";
 
 const routes = {
   guests: { label: "Participantes", icon: Group },
@@ -36,10 +37,23 @@ export const Header = ({ title }: HeaderProps) => {
           direction="row"
           justifyContent="space-between"
           alignItems="center"
-          p={{xs: 2, md: 0}}
+          p={{ xs: 2, md: 0 }}
         >
           <Stack direction="row" alignItems="center" spacing={2}>
-            <Box sx={{ backgroundColor: "#ffffff", p: 1, borderRadius: "100vmax", aspectRatio: 1, maxWidth: 25, maxHeight: 25, justifyContent: "center", alignItems: "center" }}>
+            <Box
+              component={Link}
+              href="/conta-bar"
+              sx={{
+                backgroundColor: "#ffffff",
+                p: 1,
+                borderRadius: "100vmax",
+                aspectRatio: 1,
+                maxWidth: 25,
+                maxHeight: 25,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <img src="icon.svg" alt="Conta-bar logo" width={25} height={25} />
             </Box>
             <Typography variant="h5">{title}</Typography>
@@ -68,7 +82,7 @@ export const Header = ({ title }: HeaderProps) => {
               {Object.entries(routes).map(([path, route]) => (
                 <Button
                   key={path}
-                  href={'/conta-bar/' + path}
+                  href={"/conta-bar/" + path}
                   color="inherit"
                   startIcon={<route.icon />}
                   sx={{
