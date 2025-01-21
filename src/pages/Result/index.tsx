3551b3a9442f1b72d2fb536b13258532b3dbcdb9
jonatas1n <jonatasgeek@gmail.com>
2025-01-21 01:36:09 -0300

    feat: move results function to component

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -4,29 +4,46 @@ import { useAppContext } from "../../hooks/Context";
 import Stack from "@mui/material/Stack";
 import Card from "@mui/material/Card";
 
-import { useEffect } from "react";
-import { Table, TableCell, TableRow } from "@mui/material";
+import { Table, TableBody, TableCell, TableRow } from "@mui/material";
 
 export const Result = () => {
-  const { guests, costs, products, calculateTotalCosts } = useAppContext();
+  const { guests, costs, products } = useAppContext();
 
-  useEffect(() => {
-    calculateTotalCosts();
-  }, [costs, products, guests, calculateTotalCosts]);
+  const updatedGuestsCosts = Object.values(costs).reduce<
+    Record<string, number>
+  >((acc, cost) => {
+    const total = Object.values(cost.products)
+      .filter((product) => product.productID)
+      .reduce((sum, currentValue) => {
+        const unitCost =
+          products[currentValue.productID]?.price ?? 0;
+        return sum + unitCost * currentValue.quantity;
+      }, 0);
+
+    cost.guests
+      .filter((guestID) => guests[guestID])
+      .forEach((guestID) => {
+        acc[guestID] = (acc[guestID] ?? 0) + total / cost.guests.length;
+      });
+
+    return acc;
+  }, {});
 
   return (
     <Container title="Resultados">
       <Stack spacing={2}>
         <Card>
           <Table>
-            {Object.values(guests).map((guest) => (
-              <TableRow key={guest.id}>
-                <TableCell>{guest.name}</TableCell>
-                <TableCell align="right">
-                  R$ {guest.totalCost?.toFixed(2)}
-                </TableCell>
-              </TableRow>
-            ))}
+            <TableBody>
+              {Object.entries(updatedGuestsCosts).map(([guestID, cost]) => (
+                <TableRow key={guestID}>
+                  <TableCell>{guests[guestID].name}</TableCell>
+                  <TableCell align="right">
+                    R$ {cost.toFixed(2)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
           </Table>
         </Card>
       </Stack>
