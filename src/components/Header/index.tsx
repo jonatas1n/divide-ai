e9acd014fc5c3794349896f60e8d1433a4e42081
jonatas1n <jonatasgeek@gmail.com>
2025-01-21 17:23:15 -0300

    feat: memoise header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,15 @@
+import { memo } from "react";
 import Typography from "@mui/material/Typography";
 import AppBar from "@mui/material/AppBar";
 import Stack from "@mui/material/Stack";
 
 import Container from "@mui/material/Container";
 
-import Box from "@mui/material/Box";
 import { HeaderMenu } from "./HeaderMenu";
 
 const TITLE = "Conta Bar";
 
-export const Header = () => {
+export const Header: React.FC = memo(() => {
   return (
     <AppBar position="fixed">
       <Container>
@@ -20,20 +20,18 @@ export const Header = () => {
           p={2}
         >
           <Stack direction="row" alignItems="center" spacing={2}>
-            <Box
-              sx={{
-                backgroundColor: "#ffffff",
-                p: 1,
-                borderRadius: "100vmax",
-                aspectRatio: 1,
-                maxWidth: 25,
-                maxHeight: 25,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+            <Stack
+              bgcolor="#FFFFFF"
+              borderRadius="100vmax"
+              maxWidth={25}
+              maxHeight={25}
+              justifyContent="center"
+              alignItems="center"
+              p={1}
+              sx={{ aspectRatio: 1 }}
             >
               <img src="icon.svg" alt="Conta-bar logo" width={25} height={25} />
-            </Box>
+            </Stack>
             <Typography variant="h5">{TITLE}</Typography>
           </Stack>
           <HeaderMenu />
@@ -41,4 +39,4 @@ export const Header = () => {
       </Container>
     </AppBar>
   );
-};
+});
