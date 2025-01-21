1b2e1ddaaa55ab7f4f7dbffa4ec24dfbda261118
jonatas1n <jonatasgeek@gmail.com>
2025-01-21 11:11:10 -0300

    feat: add signature

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -2,19 +2,20 @@ import { PropsWithChildren } from "react";
 import Stack from "@mui/material/Stack";
 import MUIContainer from "@mui/material/Container";
 import { Header } from "../../components/Header";
+import { Signature } from "../../components/Signature";
 
 type ContainerProps = {
-  children: ReactNode;
   title: string;
 }
 
 export const Container = ({ children, title }: PropsWithChildren<ContainerProps>) => {
   return (
-    <Stack pb={3}>
+    <Stack pb={3} minHeight="100vh">
       <Header title={title} />
       <MUIContainer sx={{ mt: 12 }}>
         {children}
       </MUIContainer>
+      <Signature />
     </Stack>
   )
 }
---
+++ src/components/Signature/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material";
+
+export const Signature = () => {
+  return (
+    <Button
+      href="https://www.linkedin.com/in/jonatasgomesdev/"
+      target="_blank"
+      color="error"
+      variant="text"
+      sx={{ mt: 2, opacity: 0.5 }}
+    >
+      Feito com ❤ por Jônatas Gomes
+    </Button>
+  );
+};
