2545e2221b8c8b11d743dc2a406efb1287a96db4
jonatas1n <jonatasgeek@gmail.com>
2025-01-22 01:01:57 -0300

    chore: remove animation from signature

--- src/components/Signature/index.tsx
+++ src/components/Signature/index.tsx
@@ -1,18 +1,15 @@
 import Button from "@mui/material/Button";
-import Fade from "@mui/material/Fade";
 
 export const Signature = () => {
   return (
-    <Fade in timeout={3000}>
-      <Button
-        href="https://www.linkedin.com/in/jonatasgomesdev/"
-        target="_blank"
-        color="error"
-        variant="text"
-        sx={{ mt: 2, opacity: 0.5 }}
-      >
-        Feito com ❤ por Jônatas Gomes
-      </Button>
-    </Fade>
+    <Button
+      href="https://www.linkedin.com/in/jonatasgomesdev/"
+      target="_blank"
+      color="error"
+      variant="text"
+      sx={{ mt: 2, opacity: 0.5 }}
+    >
+      Feito com ❤ por Jônatas Gomes
+    </Button>
   );
 };
