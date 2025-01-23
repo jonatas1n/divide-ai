aae76ab4a3f468c16cd41d307b5cfdbbc6f65161
jonatas1n <jonatasgeek@gmail.com>
2025-01-23 11:27:32 -0300

    fix: edit text

--- src/components/FormModal/index.tsx
+++ src/components/FormModal/index.tsx
@@ -10,7 +10,7 @@ export const FormModal = ({onClose, ...modalProps}: FormModalProps) => {
       <Card elevation={5} sx={{position: "absolute", margin: 2, transform: "translate(-50%, -50%)",  top: "50%", left: "50%", minWidth: "60vw"}}>
         <Stack spacing={1} p={4} textAlign="center">
           <Typography fontFamily="Lexend">
-            Em breve, lançaremos um formulário para você mandar suas dicas e sugestões.
+            Em breve, lançaremos um formulário para você mandar suas dúvidas e sugestões.
           </Typography>
           <Typography fontFamily="Lexend" fontWeight="400" variant="caption">
             Anote para quando ele estiver disponível!
