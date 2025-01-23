a630a3127dcd647b0f313aa76295c7e05bcca6c7
jonatas1n <jonatasgeek@gmail.com>
2025-01-23 10:15:13 -0300

    feat: add info card when costs is empty

--- src/pages/Costs/index.tsx
+++ src/pages/Costs/index.tsx
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import Add from "@mui/icons-material/Add";
 import Delete from "@mui/icons-material/Delete";
 import { NavigationHeader } from "../../components/NavigationHeader";
+import { InfoCard } from "../../components/InfoCard";
 
 export const Costs = () => {
   const { costs, addCost, refreshCosts, updateCosts } = useAppContext();
@@ -31,6 +32,13 @@ export const Costs = () => {
         {Object.values(costs).map((cost) => (
           <CostItem key={cost.id} cost={cost} />
         ))}
+        {Object.values(costs).length === 0 && (
+          <InfoCard>
+            Marque aqui quais comidas ou bebidas foram
+            consumidas por cada pessoa. O resultado da divisão será calculada na página
+            de resultados.
+          </InfoCard>
+        )}
         <Button variant="outlined" onClick={addCost}>
           <Add fontSize="large"/>
         </Button>
