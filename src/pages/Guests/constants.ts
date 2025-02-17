3008493e6c3020dfc1105541597186fc1d3be266
jonatas1n <jonatasgeek@gmail.com>
2025-02-17 16:40:36 -0300

    chore: change guests info message

--- src/pages/Guests/constants.ts
+++ src/pages/Guests/constants.ts
@@ -3,4 +3,4 @@ export const NEXT_OPTION_LABEL = "Produtos";
 export const GUEST_PLACEHOLDER = "Nome do participante";
 export const CLEAR_ALL_CONFIRM_MESSAGE =
   "Tem certeza de que deseja limpar todos os participantes?";
-export const INFO_GUEST_MESSAGE = "Adicione os nomes de quem vai dividir com você.";
+export const INFO_GUEST_MESSAGE = "Adicione os nomes de quem vai dividir a conta.";
